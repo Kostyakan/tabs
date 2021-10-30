a30f21c106b4ee589d013a6294b6278e9b6b8b6f
unknown <you@example.com>
2021-10-30 20:11:10 +0300

    timer ready

--- js/script.js
+++ js/script.js
@@ -28,15 +28,54 @@ window.addEventListener('DOMContentLoaded',function() {
              let target = event.target;
              if (target &&  target.classList.contains('info-header-tab')) {
                  for (let i = 0; i< tab.length; i++) {
-                    
-                     if (target === tab[i]) {
+                      if (target === tab[i]) {
                          hideTabContent(0);
                          showTabContent(i);
                          break;
                      }
                  }
              }
+             
         });
+        // Timer
+
+        let deadline ='2022-10-21';
+
+        function getTimeRemaining(endtime) {
+            let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/100/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
         
-});
+
+            return {
+                'total' : t,
+                'hors' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+        }
+
+        function setClock(id,endtime) {
+           let timer =document.getElementById(id),
+            hours =timer.querySelector('.hours'),
+            minutes =timer.querySelector('.minutes'),
+            seconds =timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock,1000);
+
+            function updateClock() {
+                let t = getTimeRemaining(endtime);
+                hours.textContent = t.hors;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+
+                if (t.total<=0) {
+                    clearInterval(timeInterval)
+                }
+            }
+        }
+
+        setClock('timer', deadline);
+
+        });
 
